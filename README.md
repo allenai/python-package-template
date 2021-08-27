00857990f1cfffb63757daa6b480f298961c539d
epwalsh <epwalsh10@gmail.com>
2021-08-27 16:20:58 -0700

    add files

---
+++ README.md
@@ -0,0 +1,14 @@
+# python-package-template
+
+This is a template repository for projects that are Python packages.
+
+## Getting started
+
+After creating a new repository from this template, you need to:
+
+1. Change the name of the `my_package` directory to the name you want.
+2. Replace all mentions of `my_package` throughout this repository with the name you want.
+
+    ```bash
+    find . -type f -not -path ./.git -not -path ./README.md | xargs grep 'my_package'
+    ```
---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+# Load requirements.txt with a special case for allennlp so we can handle
+# cross-library integration testing.
+with open("requirements.txt") as requirements_file:
+    import re
+
+    def fix_url_dependencies(req: str) -> str:
+        """Pip and setuptools disagree about how URL dependencies should be handled."""
+        m = re.match(
+            r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
+        )
+        if m is None:
+            return req
+        else:
+            return f"{m.group('name')} @ {req}"
+
+    install_requirements = []
+    for line in requirements_file:
+        line = line.strip()
+        if line.startswith("#") or len(line) <= 0:
+            continue
+        install_requirements.append(fix_url_dependencies(line))
+
+setup(
+    name="my_package",
+    version="0.0.1",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="",
+    url="",
+    author="Allen Institute for Artificial Intelligence",
+    author_email="contact@allenai.org",
+    license="Apache",
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
+    ),
+    install_requires=install_requirements,
+    python_requires=">=3.6.1",
+)
