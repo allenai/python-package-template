f943db4d82b32069606aaeaf94e96870c3bc63ea
epwalsh <epwalsh10@gmail.com>
2022-03-02 09:45:21 -0800

    require python >= 3.7

--- setup.py
+++ setup.py
@@ -54,5 +54,5 @@ setup(
     package_data={"my_package": ["py.typed"]},
     install_requires=read_requirements("requirements.txt"),
     extras_require={"dev": read_requirements("dev-requirements.txt")},
-    python_requires=">=3.6.1",
+    python_requires=">=3.7",
 )
