f73a053fcb4f0efd31b4d80df563c6ff65080f18
TheRealKeto <therealketo@gmail.com>
2022-06-19 17:04:00 -0400

    Allow accessing version variables after initialization
    This commit does two things:
    
    1. Version variables are accessable by users after
       package initialization.
    
       Users can now use my_package.VERSION over exporting
       my_package.version, then accessing the version value,
       which makes things much easier.
    
    2. Adapt changes made to version values to other parts
       of the project
    
       Version variables were renamed to the following:
    
            VERSION_SHORT ==> VERSION
            VERSION       ==> RELEASE
    
        and parts referring to version values were changed
        accordingly, so nothing's broken.
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,15 +18,15 @@ from datetime import datetime
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-from my_package.version import VERSION, VERSION_SHORT  # noqa: E402
+from my_package import RELEASE, VERSION  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
 project = "my-package"
 copyright = f"{datetime.today().year}, Allen Institute for Artificial Intelligence"
 author = "Allen Institute for Artificial Intelligence"
-version = VERSION_SHORT
-release = VERSION
+version = VERSION
+release = RELEASE
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("my_package/version.py", "r") as version_file:
 
 setup(
     name="my-package",
-    version=VERSION["VERSION"],
+    version=VERSION["RELEASE"],
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
